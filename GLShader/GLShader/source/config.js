129dc6d068a64b9b4b884d759fcec8db1ff5a766
vegerjiang <jackhenizaiyiqi@qq.com>
2014-06-04 21:58:36 +0800

    js数据

---
+++ GLShader/GLShader/source/config.js
@@ -0,0 +1,26 @@
+[
+ {
+    "glsl":"book/book.glsl",
+    "thumb":"book/book_thumb.jpg",
+    "background":"book/book.jpg",
+    "mask":"book/book_mask.png"
+ },
+ {
+    "glsl":"coffee/coffee.glsl",
+    "thumb":"coffee/coffee_thumb.jpg",
+    "background":"coffee/coffee.jpg",
+    "mask":"coffee/coffee_mask.png"
+ },
+ {
+    "glsl":"draw/draw.glsl",
+    "thumb":"draw/draw_thumb.jpg",
+    "background":"draw/draw.jpg",
+    "mask":"draw/draw_mask.png"
+ },
+ {
+    "glsl":"poster/poster.glsl",
+    "thumb":"poster/poster_thumb.jpg",
+    "background":"poster/poster.jpg",
+    "mask":"poster/poster_mask.png"
+ }
+ ]
